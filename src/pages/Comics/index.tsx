1c58559d736046b3b9f58b1491d8c9ef5f574472
Bruno <brunnofn19@gmail.com>
2020-08-18 02:48:00 -0300

    add new layout

--- src/pages/Comics/index.tsx
+++ src/pages/Comics/index.tsx
@@ -1,9 +1,101 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { FlatList, Image } from 'react-native';
 
-import { Container } from './styles';
+import marvelLogo from '../../assets/marvel-logo.png';
+import { api } from '../../services/api';
+import {
+  Container,
+  Header,
+  Title,
+  ComicCard,
+  ComicContent,
+  ComicName,
+  IssueNumberText,
+  ComicPrice,
+} from './styles';
 
-const Comics: React.FC = () => {
-  return <Container />;
+interface RouteProps {
+  route: {
+    params: {
+      hero_id: string;
+    };
+  };
+}
+
+interface HeroComicsState {
+  id: number;
+  title: string;
+  issueNumber: number;
+  price: number;
+  thumbnail: string;
+}
+
+type HeroComicsResponse = Omit<HeroComicsState, 'price'> & {
+  prices: Array<{ price: number }>;
+  thumbnail: { path: string };
+};
+
+const Comics: React.FC<RouteProps> = ({ route }) => {
+  const [heroComics, setHeroComics] = useState<HeroComicsState[]>([]);
+
+  useEffect(() => {
+    api
+      .get(`/characters/${route.params.hero_id}/comics`, {
+        params: {
+          ts: '1',
+          apikey: '374d072e930a8a8d1e11aa1ef651c693',
+          hash: '5ca7dfe0900ced627d239fa684227b4a',
+        },
+      })
+      .then(response => {
+        const { data } = response;
+
+        const eachComic = data.data.results;
+
+        const formattedComic = eachComic.map((each: HeroComicsResponse) => ({
+          id: each.id,
+          title: each.title,
+          issueNumber: each.issueNumber,
+          price: each.prices[0].price,
+          thumbnail: each.thumbnail.path,
+        }));
+
+        setHeroComics(formattedComic);
+      });
+  }, [route.params.hero_id]);
+
+  return (
+    <Container>
+      <Header>
+        <Image source={marvelLogo} />
+
+        <Title>List of your favorite hero's comics</Title>
+      </Header>
+
+      <FlatList
+        data={heroComics}
+        style={{ width: '100%' }}
+        showsVerticalScrollIndicator={false}
+        keyExtractor={comic => String(comic.id)}
+        renderItem={({ item: comic }) => (
+          <ComicCard>
+            <Image
+              source={{
+                uri: `${comic.thumbnail}.jpg`,
+              }}
+              style={{ width: 100, height: 200, resizeMode: 'center' }}
+            />
+
+            <ComicContent>
+              <ComicName>{comic.title}</ComicName>
+              <IssueNumberText>IN: {comic.issueNumber}</IssueNumberText>
+              <ComicPrice>${comic.price}</ComicPrice>
+            </ComicContent>
+          </ComicCard>
+        )}
+      />
+    </Container>
+  );
 };
 
 export { Comics };
--- src/pages/Comics/styles.ts
+++ src/pages/Comics/styles.ts
@@ -2,7 +2,66 @@ import styled from 'styled-components/native';
 
 export const Container = styled.View`
   flex: 1;
-  background: #282828;
+  background: #000;
   justify-content: center;
   align-items: center;
+  padding: 60px 30px 0;
+`;
+
+export const Header = styled.View`
+  width: 100%;
+  padding: 30px 0 0;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const Title = styled.Text`
+  font-size: 15px;
+  color: #fffde4;
+  font-weight: bold;
+  text-align: center;
+  text-transform: uppercase;
+  letter-spacing: 1px;
+  margin: 10px 0 20px;
+`;
+
+export const ComicCard = styled.View`
+  width: 100%;
+  padding: 0 15px;
+  border-radius: 5px;
+  justify-content: space-between;
+  align-items: center;
+  flex-direction: row;
+  background: #111111;
+  margin-bottom: 10px;
+`;
+
+export const ComicContent = styled.View`
+  flex: 1;
+  height: 150px;
+  justify-content: center;
+  padding-left: 15px;
+`;
+
+export const ComicName = styled.Text`
+  font-size: 14px;
+  font-weight: bold;
+  color: #fffde4;
+`;
+
+export const IssueNumberText = styled.Text`
+  font-size: 13px;
+  font-weight: bold;
+  color: #a78b70;
+  margin: 10px 0;
+`;
+
+export const ComicPrice = styled.Text`
+  font-size: 18px;
+  background: #a78b70;
+  padding: 10px 5px;
+  border-radius: 5px;
+  text-align: center;
+  font-weight: bold;
+  color: #fffde4;
 `;
